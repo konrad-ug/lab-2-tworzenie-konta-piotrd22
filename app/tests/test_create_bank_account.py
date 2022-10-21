6f329eb3f5f5c77dea7a13f594b98eea17be599e
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-10-21 17:09:31 +0200

    feature-3

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -10,7 +10,7 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
         self.assertEqual(pierwsze_konto.pesel, "12345678900", "PESEL nie został zapisany")
-        self.assertEqual(len(pierwsze_konto.pesel), 11, "PESEL jest za krotkiw")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Niepoprwany pesel")
 
 
 
