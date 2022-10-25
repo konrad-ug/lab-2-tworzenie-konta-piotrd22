f785735c98c19b1287fc3a9f6f17136b87a0efe9
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-10-25 11:09:58 +0200

    feature3

--- app/Konto.py
+++ app/Konto.py
@@ -2,14 +2,21 @@ class Konto:
     def __init__(self, imie, nazwisko, pesel, kod_prom = None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
         self.is_pesel(pesel)
-
+        self.is_promo(kod_prom)
     
     def is_pesel(self, pesel):
         if(len(pesel) == 11):
             self.pesel = pesel
         else:
             self.pesel = "Niepoprwany pesel!"
+    
+    def is_promo(self, kod_prom):
+        if(kod_prom != None and kod_prom[:5] == "PROM_" and len(kod_prom) == 8):
+            self.saldo = 50
+
+        else:
+            self.saldo = 0
+
     
     
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -8,12 +8,17 @@ class TestCreateBankAccount(unittest.TestCase):
         pierwsze_konto = Konto("Dariusz", "Januszewski", "12345678900")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+
+        #PESEL testing
         self.assertEqual(pierwsze_konto.pesel, "12345678900", "PESEL nie został zapisany")
         self.assertEqual(len(pierwsze_konto.pesel), 11, "Niepoprwany pesel")
 
-        # drugie_konto = Konto("Marek", "Papszun", "98765432100", "PROM_XYZ")
-        # self.assertEqual(drugie_konto.saldo, 50, "Niepoprwany kod promocyjny")
+        #Checking if valid promo code add 50 to saldo
+        drugie_konto = Konto("Marek", "Papszun", "98765432100", "PROM_xyz")
+        self.assertEqual(drugie_konto.saldo, 50, "Niepoprwany kod promocyjny")
 
-
-    #tutaj proszę dodawać nowe testy
+        #Checking if invalid promo code add nothing
+        trzecie_konto = Konto("Marek", "Papszun", "98765678910", "PORN_xyz")
+        self.assertEqual(trzecie_konto.saldo, 0, "Niepoprwany kod promocyjny")
