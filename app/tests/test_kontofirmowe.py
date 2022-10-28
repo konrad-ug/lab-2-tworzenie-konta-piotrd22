42c87b5e83788316732d83ea034ad53e4d92173b
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-10-28 17:28:53 +0200

    rozbicie-kont

--- app/tests/test_kontofirmowe.py
+++ app/tests/test_kontofirmowe.py
@@ -22,21 +22,3 @@ class TestKotoFirmowe(unittest.TestCase):
         konto2 = KontoFirmowe(self.nazwa, self.invalid_nip2)
         self.assertEqual(konto1.NIP, "Niepoprawny NIP!", "Niepoprawny NIP przechodzi!")
         self.assertEqual(konto2.NIP, "Niepoprawny NIP!", "Niepoprawny NIP przechodzi!")
-
-    def test_transfer_from(self):
-        konto = KontoFirmowe(self.nazwa, self.NIP)
-        konto.saldo = 400
-        konto.transfer_from(100)
-        self.assertEqual(konto.saldo, 400 + 100, "Niepoprawne księgowanie!")
-
-    def test_transfer_to(self):
-        konto = KontoFirmowe(self.nazwa, self.NIP)
-        konto.saldo = 400
-        konto.transfer_to(100)
-        self.assertEqual(konto.saldo, 400 - 100, "Niepoprawne księgowanie!")
-    
-    def test_if_i_have_enough_money(self):
-        konto = KontoFirmowe(self.nazwa, self.NIP)
-        konto.saldo = 200
-        konto.transfer_to(300)
-        self.assertEqual(konto.saldo, 200, "Konto wykonuje przelew pomimo za małego balansu!")
