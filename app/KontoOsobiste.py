13459d8d8cfc532fc29da412dba74e60a7b77ecd
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-11-05 12:01:23 +0100

    feature11-some-upgrades

--- app/KontoOsobiste.py
+++ app/KontoOsobiste.py
@@ -4,6 +4,8 @@ class KontoOsobiste(Konto):
     def __init__(self, imie, nazwisko, pesel, kod_prom = None):
         self.imie = imie
         self.nazwisko = nazwisko
+        self.fast_t_cost = 1
+        self.historia = []
 
         self.is_pesel(pesel)
         self.is_promo(kod_prom)
@@ -23,9 +25,3 @@ class KontoOsobiste(Konto):
 
         else:
             self.saldo = 0
-    
-    def transfer_to_fast(self, sum, x = 1):
-        if(self.saldo - sum - x >= -x):
-            self.saldo -= (sum + x)
-        else:
-            self.saldo = self.saldo
