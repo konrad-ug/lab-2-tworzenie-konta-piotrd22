ff784f48aab6520766dea5d34dc08745b0e1295b
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-11-19 15:43:20 +0100

    feature-13

--- app/KontoFirmowe.py
+++ app/KontoFirmowe.py
@@ -1,5 +1,6 @@
 from app.Konto import Konto
 
+
 class KontoFirmowe(Konto):
     def __init__(self, nazwa_firmy, NIP):
         self.nazwa_firmy = nazwa_firmy
@@ -8,10 +9,29 @@ class KontoFirmowe(Konto):
         self.historia = []
 
         self.is_nip(NIP)
-    
-    
+
     def is_nip(self, NIP):
-        if(len(NIP) == 10 and NIP.isdigit()):
+        if (len(NIP) == 10 and NIP.isdigit()):
             self.NIP = NIP
         else:
             self.NIP = "Niepoprawny NIP!"
+
+    def check_balance_to_loan(self, suma):
+        if (self.saldo >= 2 * suma):
+            return True
+        else:
+            return False
+
+    def check_history_to_loan(self):
+        filtered_history = filter(lambda x: x == -1775, self.historia)
+        if (len(list(filtered_history)) >= 1):
+            return True
+        else:
+            return False
+
+    def zaciagnij_kredyt(self, suma):
+        if (self.check_history_to_loan() and self.check_balance_to_loan(suma)):
+            self.saldo += suma
+            return True
+        else:
+            return False
---
+++ app/tests/test_company_loan.py
@@ -0,0 +1,24 @@
+import unittest
+from parameterized import parameterized
+from app.KontoFirmowe import KontoFirmowe
+
+
+class TestCompanyLoan(unittest.TestCase):
+
+    def setUp(self):
+        self.konto = KontoFirmowe("POLPLEX", "1234567890")
+
+    @parameterized.expand([
+        ([-1775, 500, 100], 1000, 500, True, 1500),
+        ([-100, 100], 1000, 500, False, 1000),
+        ([100, -1775, 1775], 1001, 500, True, 1501),
+        ([-1775], 999, 500, False, 999),
+        ([500, 200], 12000, 500, False, 12000),
+        ([-1775], 200, 500, False, 200)
+    ])
+    def test_company_loan(self, historia, saldo, suma, werdykt, saldo_po):
+        self.konto.historia = historia
+        self.konto.saldo = saldo
+        is_loan = self.konto.zaciagnij_kredyt(suma)
+        self.assertEqual(is_loan, werdykt)
+        self.assertEqual(self.konto.saldo, saldo_po)
