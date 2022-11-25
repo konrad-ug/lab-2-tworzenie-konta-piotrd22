f8da7fda45abf5cee7d56fcb3f8451a2e009e12b
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-11-25 16:24:27 +0100

    added test to rejestrkont

--- app/tests/test_rejestrkont.py
+++ app/tests/test_rejestrkont.py
@@ -18,9 +18,16 @@ class TestRegister(unittest.TestCase):
         self.assertEqual(RejestrKont.usersCount(), 1)
 
     def test_2_add_second_user(self):
-        user = KontoOsobiste(self.name, self.surname, "1234567890")
+        user = KontoOsobiste(self.name + "as", self.surname, "03225432100")
         RejestrKont.addUser(user)
         self.assertEqual(RejestrKont.usersCount(), 2)
+
+    def test_searchUserbyPesel(self):
+        self.assertEqual(RejestrKont.searchUserbyPesel(
+            "02225432100").imie, "Marek")
+        self.assertEqual(RejestrKont.searchUserbyPesel(
+            "03225432100").imie, "Marekas")
+        self.assertEqual(RejestrKont.searchUserbyPesel("1234567890"), None)
 
     @classmethod
     def tearDownClass(cls):
