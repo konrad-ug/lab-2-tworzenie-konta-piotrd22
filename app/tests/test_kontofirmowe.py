97b50d370de2c79e0141b70c2edd606b96c53639
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-12-09 15:24:41 +0100

    tried added mock

--- app/tests/test_kontofirmowe.py
+++ app/tests/test_kontofirmowe.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from app.KontoFirmowe import KontoFirmowe
 
@@ -7,8 +7,24 @@ from app.KontoFirmowe import KontoFirmowe
 class TestKotoFirmowe(unittest.TestCase):
     nazwa = "POLPLEX"
     NIP = "1234567890"
-    invalid_nip = "123456789"
-    invalid_nip2 = "123456789a"
+
+    def _mock_response(self, status):
+        return Mock(status_code=status)
+
+    @patch('requests.get')
+    def test_invalid_nip(self, mock_get):
+        mock_res = self._mock_response(status=400)
+        mock_get.return_value = mock_res
+        konto1 = KontoFirmowe(self.nazwa, self.NIP)
+        self.assertEqual(konto1.NIP, "Niepoprawny NIP!",
+                         "Niepoprawny NIP przechodzi!")
+
+    @patch('requests.get')
+    def test_valid_nip(self, mock_get):
+        mock_res = self._mock_response(status=200)
+        mock_get.return_value = mock_res
+        konto = KontoFirmowe(self.nazwa, self.NIP)
+        self.assertEqual(konto.NIP, self.NIP, "Niepoprawnie zapisany NIP!")
 
     @patch('app.KontoFirmowe.KontoFirmowe.is_nip_real')
     def test_creation(self, mock_is_nip_real):
@@ -17,17 +33,3 @@ class TestKotoFirmowe(unittest.TestCase):
         self.assertEqual(konto.nazwa_firmy, self.nazwa,
                          "Niepoprawna nazwa firmy!")
         self.assertEqual(konto.saldo, 0, "Saldo po zainicjowaniu != 0")
-
-    @patch('app.KontoFirmowe.KontoFirmowe.is_nip_real')
-    def test_valid_nip(self, mock_is_nip_real):
-        mock_is_nip_real.return_value = True
-        konto = KontoFirmowe(self.nazwa, self.NIP)
-        self.assertEqual(konto.NIP, self.NIP, "Niepoprawnie zapisany NIP!")
-
-    def test_invalid_nip(self):
-        konto1 = KontoFirmowe(self.nazwa, self.invalid_nip)
-        konto2 = KontoFirmowe(self.nazwa, self.invalid_nip2)
-        self.assertEqual(konto1.NIP, "Niepoprawny NIP!",
-                         "Niepoprawny NIP przechodzi!")
-        self.assertEqual(konto2.NIP, "Niepoprawny NIP!",
-                         "Niepoprawny NIP przechodzi!")
